e0365ef1984185ad0cea2162b0214bdebffcb4cd
qlqldldh <60098657+qlqldldh@users.noreply.github.com>
2020-05-26 14:54:08 +0900

    Create JsonServlet.java

---
+++ jsp_study/webTechBookContents/JsonServlet.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class JsonServlet1
+ */
+@WebServlet("/json")
+public class JsonServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		
+		String jsonInfo=request.getParameter("jsonInfo");
+		PrintWriter out = response.getWriter();
+		try {
+			JSONParser jsonParser = new JSONParser();
+			JSONObject jsonObject=(JSONObject) jsonParser.parse(jsonInfo);
+			System.out.println("* member Informations");
+			
+			System.out.println(jsonObject.get("name"));
+			System.out.println(jsonObject.get("age"));
+			System.out.println(jsonObject.get("gender"));
+			System.out.println(jsonObject.get("nickname"));
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
