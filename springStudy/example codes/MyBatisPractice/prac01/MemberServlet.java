3ed7527c6ac931b51ecf993fca1102fa91c9fa38
qlqldldh <60098657+qlqldldh@users.noreply.github.com>
2020-06-18 15:52:49 +0900

    update exmples of mybatis

---
+++ springStudy/example codes/MyBatisPractice/prac01/MemberServlet.java
@@ -0,0 +1,35 @@
+package com.spring.ex01;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+/**
+ * Servlet implementation class MemberServlet
+ */
+@WebServlet("/mem.do")
+public class MemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		
+		MemberDAO dao = new MemberDAO();
+		List<MemberVO> membersList = dao.selectAllMemberList();
+		request.setAttribute("membersList", membersList);
+		RequestDispatcher dispatch = request.getRequestDispatcher("listMembers.jsp");
+		dispatch.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
